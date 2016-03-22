8865be6a39e02a7b8a985c2479c48ea9a78537ef
Maxat Kulmanov <coolmaksat@gmail.com>
2016-03-22 21:42:27 +0300

    Initial Web app with spring-boot

---
+++ src/main/java/sa/edu/kaust/Rdf2owlApplication.java
@@ -0,0 +1,12 @@
+package sa.edu.kaust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rdf2owlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rdf2owlApplication.class, args);
+	}
+}
